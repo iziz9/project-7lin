027921d7b9a2399b08acc5ec8bce9cefee18eb43
Jaeheon-So <sjh-98@naver.com>
2023-03-22 16:50:38 +0900

    feat: 로그인 페이지 대략적인 마크업

--- src/commons/Button.tsx
+++ src/commons/Button.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import styled from "styled-components";
 
-const Button = () => {
-  return <div>Button</div>;
-};
-
-export default Button;
+export const BasicBtn = styled.button<{
+  backgroundColor?: string;
+  fontColor?: string;
+  isDeskTop: boolean;
+}>`
+  width: ${(props) => (props.isDeskTop ? "700px" : "248px")};
+  height: ${(props) => (props.isDeskTop ? "65px" : "48px")};
+  border: none;
+  border-radius: 8px;
+  font-size: ${(props) => (props.isDeskTop ? "23px" : "16px")};
+  font-weight: bold;
+  text-align: center;
+  color: ${(props) => props.fontColor || "#fff"};
+  background-color: ${(props) => props.backgroundColor || "#0D99FF"};
+  cursor: pointer;
+`;
