b7c61b017d8476aefe5d1070768b56b7668e42dd
Hyeeeein <khi_9866@naver.com>
2023-03-23 17:51:28 +0900

    fix: conflict

--- src/pages/review/ReviewItems.tsx
+++ src/pages/review/ReviewItems.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import test from "/test.png";
+import test from "/review.png";
 
 const ReviewItems = () => {
   const data = new Array();
