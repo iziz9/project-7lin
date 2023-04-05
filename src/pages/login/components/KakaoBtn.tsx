2b1a3a4976fcccc30660d37ca8d98f719da185b4
Jaeheon-So <sjh-98@naver.com>
2023-04-05 23:40:52 +0900

    feat: test commit

--- src/pages/login/components/KakaoBtn.tsx
+++ src/pages/login/components/KakaoBtn.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
-type Props = {};
-
-const KakaoBtn = (props: Props) => {
+const KakaoBtn = () => {
   const apiKey = import.meta.env.VITE_KAKAO_API_KEY;
   const redirectUrl = import.meta.env.VITE_KAKAO_REDIRECT_URL;
   const authUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${apiKey}&redirect_uri=${redirectUrl}&response_type=code`;
